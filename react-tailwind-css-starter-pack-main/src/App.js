ee8f4d2588577836d65b6d5d284214422cbc85c8
manya <manya1610@gmail.com>
2024-07-02 13:51:01 +0530

    first commit

---
+++ react-tailwind-css-starter-pack-main/src/App.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  
+  const[count , setCount]=useState(0);
+
+  function decreaseHandler(){
+    setCount(count-1);
+  }
+
+  function increaseHandler(){
+    setCount(count+1);
+  }
+
+  function resetHandler()
+  {
+    setCount(0);
+  }
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+      <div className="text-[#0398d4] font-medium text-2xl">
+        Increment & Decrement
+      </div>
+      <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+        <button onClick={decreaseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          -
+        </button>
+        <div className="font-bold gap-12 text-4xl pt-1">
+          {count}
+        </div>
+        <button onClick={increaseHandler} className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          +
+        </button>
+      </div>
+      <button onClick={resetHandler} className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg">
+        reset
+      </button>
+    </div>
+  );
+}
+
+export default App;
